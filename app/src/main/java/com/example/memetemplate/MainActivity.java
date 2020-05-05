1a2a0d2aead5d25baf3aecb5079830787f9e81bd
alevi0106 <alevi0106@gmail.com>
2020-05-05 16:31:20 +0700

    Added List View in App

--- app/src/main/java/com/example/memetemplate/MainActivity.java
+++ app/src/main/java/com/example/memetemplate/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.memetemplate;
+import com.example.memetemplate.CustomCountryList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private String countryNames[] = {
+            "India",
+            "China",
+            "Nepal",
+            "Bhutan"
+    };
+
+    private String capitalNames[] = {
+            "Delhi",
+            "Beijing",
+            "Kathmandu",
+            "Thimphu"
+    };
+
+
+    private Integer imageid[] = {
+            R.drawable.india,
+            R.drawable.china,
+            R.drawable.nepal,
+            R.drawable.bhutan
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=(ListView) findViewById(R.id.country_list);
+
+
+        // For populating list data
+        CustomCountryList customCountryList = new CustomCountryList(this, countryNames, capitalNames, imageid);
+        listView.setAdapter(customCountryList);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getApplicationContext(),"You Selected "+countryNames[position-1]+ " as Country",Toast.LENGTH_SHORT).show();        }
+        });
     }
 }
